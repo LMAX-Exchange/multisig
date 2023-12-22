ed7f80f5723c792953215bb4ebe00ad26f09615a
healeyr <healeyr@lmax.com>
2023-12-22 15:37:57 +0000

    rewrite tests in typescript with auto set up of validator and dsl
    Co-authored-by: ags <ags@lmax.com>

---
+++ tests/mutisigTest.ts
@@ -0,0 +1,109 @@
+import assert = require("assert");
+import { setUpValidator } from "./utils/before";
+import { AnchorProvider, BN, Program } from "@coral-xyz/anchor";
+import {
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import { MultisigAccount, MultisigDsl } from "./utils/multisigDsl";
+import { describe } from "mocha";
+import { ChildProcess } from "node:child_process";
+
+describe("Test multisig", async () => {
+  let provider: AnchorProvider;
+  let program: Program;
+  let validatorProcess: ChildProcess;
+  let dsl: MultisigDsl;
+  before(async () => {
+    let result = await setUpValidator();
+    program = result.program;
+    provider = result.provider;
+    validatorProcess = result.validatorProcess;
+    dsl = new MultisigDsl(program);
+  });
+
+  it("should create multisig account", async () => {
+    const ownerA = Keypair.generate();
+
+    const ownerB = Keypair.generate();
+    const ownerC = Keypair.generate();
+    const owners = [ownerA.publicKey, ownerB.publicKey, ownerC.publicKey];
+    const multisigSize = 200; // Big enough.
+    const threshold = new BN(2);
+
+    const multisig: MultisigAccount = await dsl.createMultisig(
+      owners,
+      multisigSize,
+      threshold
+    );
+
+    let actualMultisig = await program.account.multisig.fetch(multisig.address);
+    assert.strictEqual(actualMultisig.nonce, multisig.nonce);
+    assert.ok(multisig.threshold.eq(actualMultisig.threshold));
+    assert.deepStrictEqual(actualMultisig.owners, multisig.owners);
+    assert.ok(actualMultisig.ownerSetSeqno === 0);
+  });
+
+  it("should perform instructions if reached multisig threshold", async () => {
+    const ownerA = Keypair.generate();
+    const ownerB = Keypair.generate();
+    const ownerC = Keypair.generate();
+    const owners = [ownerA.publicKey, ownerB.publicKey, ownerC.publicKey];
+    const multisigSize = 200; // Big enough.
+    const threshold = new BN(2);
+
+    const multisig: MultisigAccount = await dsl.createMultisig(
+      owners,
+      multisigSize,
+      threshold
+    );
+
+    // Fund the multisig signer account
+    await provider.sendAndConfirm(
+      new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: provider.publicKey,
+          lamports: new BN(1_000_000_000),
+          toPubkey: multisig.signer,
+        })
+      )
+    );
+
+    // Create instruction to send funds from multisig
+    let transactionInstruction = SystemProgram.transfer({
+      fromPubkey: multisig.signer,
+      lamports: new BN(1_000_000_000),
+      toPubkey: provider.publicKey,
+    });
+
+    let beforeBalance = await provider.connection.getBalance(
+      multisig.signer,
+      "confirmed"
+    );
+    assert.strictEqual(beforeBalance, 1_000_000_000);
+
+    const transactionAddress: PublicKey = await dsl.proposeTransaction(
+      ownerA,
+      transactionInstruction,
+      multisig.address,
+      1000
+    );
+
+    await dsl.approveTransaction(ownerB, multisig.address, transactionAddress);
+
+    await dsl.executeTransaction(
+      transactionAddress,
+      transactionInstruction,
+      multisig.signer,
+      multisig.address
+    );
+
+    let afterBalance = await provider.connection.getBalance(
+      multisig.signer,
+      "confirmed"
+    );
+    assert.strictEqual(afterBalance, 0);
+  });
+});
---
+++ tests/utils/before.ts
@@ -0,0 +1,101 @@
+import * as toml from "toml";
+import * as fs from "fs";
+import { mkdtemp } from "fs/promises";
+import { ChildProcess, exec } from "node:child_process";
+import * as os from "os";
+import { Keypair, PublicKey, Connection } from "@solana/web3.js";
+import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
+import path = require("node:path");
+import shell from 'shelljs';
+
+export interface AnchorConfig {
+  path: {
+    idl_path: string;
+    binary_path: string;
+    key_path: string;
+  };
+  provider: {
+    cluster: string;
+    wallet: string;
+  };
+  programs: {
+    localnet: {
+      coral_multisig: string;
+    };
+  };
+  validator: {
+    ledger_dir: string;
+  };
+}
+
+const PATH_TO_ANCHOR_CONFIG: string = "./Anchor.toml";
+
+export const setUpValidator = async (): Promise<{
+  provider: AnchorProvider;
+  program: Program;
+  validatorProcess: ChildProcess;
+}> => {
+  const config = readAnchorConfig(PATH_TO_ANCHOR_CONFIG);
+  const ledgerDir = await mkdtemp(path.join(os.tmpdir(), "ledger-"));
+  const user = loadKeypair(config.provider.wallet);
+  const programAddress = new PublicKey(config.programs.localnet.coral_multisig);
+
+  const internalController: AbortController = new AbortController();
+  const { signal } = internalController;
+
+  let validatorProcess = exec(
+    `solana-test-validator --ledger ${ledgerDir} --mint ${user.publicKey} --bpf-program ${config.programs.localnet.coral_multisig} ${config.path.binary_path}`,
+    { signal }
+  );
+
+  const connection = new Connection("http://127.0.0.1:8899", "confirmed");
+  const provider = new AnchorProvider(connection, new Wallet(user), {});
+
+  let attempts = 0;
+  while (true) {
+    attempts += 1;
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      let program = await connection.getAccountInfo(programAddress);
+      if (program !== null && program.executable) {
+        break;
+      }
+    } catch (e) {
+      // Bound the number of retries so the tests don't hang if there's some problem blocking
+      // the connection to the validator.
+      if (attempts == 30) {
+        console.log(
+          `Failed to start validator or connect to running validator. Caught exception: ${e}`
+        );
+        throw e;
+      }
+    }
+  }
+
+  const program = new Program(
+    JSON.parse(fs.readFileSync(config.path.idl_path).toString()),
+    programAddress,
+    provider
+  );
+
+  console.log(`anchor idl init -f ${config.path.idl_path} ${programAddress.toBase58()}  --provider.cluster ${
+      connection.rpcEndpoint
+  }`)
+  shell.exec(
+      `anchor idl init -f ${config.path.idl_path} ${programAddress.toBase58()}  --provider.cluster ${
+          connection.rpcEndpoint
+      }`
+  );
+
+  return { provider, program, validatorProcess };
+};
+
+export function readAnchorConfig(pathToAnchorToml: string): AnchorConfig {
+  return toml.parse(fs.readFileSync(pathToAnchorToml).toString());
+}
+
+export function loadKeypair(path: string) {
+  return Keypair.fromSecretKey(
+    new Uint8Array(JSON.parse(fs.readFileSync(path, "utf-8")))
+  );
+}
